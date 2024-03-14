3b07affc5299a518f0e42258eeeb28a349d7392c
ubaklan <ubaklan@threecolts.com>
2024-03-14 18:43:48 +0100

    abs path

--- startup_script.sh
+++ startup_script.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-source /home/vladimir/web-proxy/venv/bin/activate
+cd /home/vladimir/web-proxy
+
+source venv/bin/activate
 pip3 install -r /home/vladimir/web-proxy/requirements.txt
-touch /home/vladimir/web-proxy/hello.txt
-nohup python3 /home/vladimir/web-proxy/server.py > log.log &
+#touch /home/vladimir/web-proxy/hello.txt
+nohup python3 server.py > log.log &
