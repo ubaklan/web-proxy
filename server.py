763922e487573a87d455865fb0f8841d74db09a0
ubaklan <ubaklan@threecolts.com>
2024-03-15 21:47:02 +0100

    add validation

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import requests
 from http.client import HTTPConnection
 import socket
 import netifaces as ni
+import threading
 
 
 def drop_accept_encoding_on_putheader(http_connection_putheader):
@@ -48,10 +49,14 @@ def proxy(interface):
 def restart(interface):
     try:
         print(f'Sending reboot request to {interface}')
-        response = get_session(interface).post('http://192.168.100.1/ajax', json={'funcNo': '1013'})
-        return jsonify(body=response.text)
+        threading.Thread(target=send_restart_request)
+        return jsonify(status='async'), 200
     except Exception as e:
         return jsonify(error=str(e)), 500
+
+
+def send_restart_request(interface):
+    get_session(interface).post('http://192.168.100.1/ajax', json={'funcNo': '1013'})
 
 
 @app.route('/interfaces', methods=['GET'])
