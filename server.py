e9c08e532485661b10b54d0bfc31eb46b65c4e87
Dzmitry Dzeravianka <dzeravia@Dmitrys-MacBook-Pro.local>
2024-06-18 15:38:02 +0300

    Fix json error

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def proxy(interface):
     try:
         print(f'Sending request to {url} with headers {headers} to interface {interface}')
         response = get_session(interface).get(url, headers=headers, allow_redirects=True, timeout=10)
-        return ({'headers' : response.headers, 'body' : jsonify(body=response.text)})
+        return ({'headers' : str(response.headers), 'body' : jsonify(body=response.text)})
     except Exception as e:
         return jsonify(error=str(e)), 500
 
