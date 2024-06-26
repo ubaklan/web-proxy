03b28300d213bbc2c79330279d501a5d539ce119
Dzmitry Dzeravianka <dzeravia@Dmitrys-MacBook-Pro.local>
2024-06-26 21:27:02 +0700

    skip scheduling ip update

--- server.py
+++ server.py
@@ -121,5 +121,5 @@ def schedule_proxy_ip_update():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=3000)
-    schedule_proxy_ip_update()
+#     schedule_proxy_ip_update()
 
