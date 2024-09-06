511abd84f50e1bd2d1940cf7bf6e7b0b475f71e9
ubaklan <ubaklan@threecolts.com>
2024-09-06 22:50:33 +0200

    WIP

--- scrapers/consumer.py
+++ scrapers/consumer.py
@@ -75,8 +75,7 @@ def get_session(interface):
 
 def split_list(lst, n):
     k, m = divmod(len(lst), n)
-    return (lst[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
-
+    return (lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 
 def process_categories(iface, iface_categories, user_agents):
