9219cab67ec311dbc68c9163d25933be5b86c979
ubaklan <ubaklan@threecolts.com>
2024-09-06 21:46:47 +0200

    WIP

--- scrapers/consumer.py
+++ scrapers/consumer.py
@@ -1,10 +1,8 @@
 import socket
-
 import netifaces as ni
 import requests
 import threading
 import random
-
 from bs4 import BeautifulSoup
 import json
 import time
@@ -59,14 +57,11 @@ def is_interface_alive(interface):
 def get_network_interfaces():
     interfaces = ni.interfaces()
     interface_info = []
-
     for interface in interfaces:
         addrs = ni.ifaddresses(interface)
         addr = addrs.get(ni.AF_LINK)[0]['addr'] if ni.AF_LINK in addrs else None
-
         if ni.AF_INET in addrs and addr is not None and interface != 'wlan0' and is_interface_alive(interface):
             interface_info.append({"name": interface, "addr": addr})
-
     return interface_info
 
 
@@ -84,15 +79,18 @@ def split_list(lst, n):
 
 
 def process_categories(iface, iface_categories, user_agents):
-    thread_name = threading.current_thread().name
-    thread_id = threading.get_native_id()
-
     threads = []
+    results = []
+
+    def thread_target(category, user_agent):
+        result = scrape_category(iface, category, user_agent)
+        if result:
+            results.append(result)
 
     for category in iface_categories:
         thread = threading.Thread(
-            target=scrape_category,
-            args=(iface, category, random.choice(user_agents))
+            target=thread_target,
+            args=(category, random.choice(user_agents))
         )
         threads.append(thread)
 
@@ -102,11 +100,13 @@ def process_categories(iface, iface_categories, user_agents):
     for thread in threads:
         thread.join()
 
+    return results
+
 
 def scrape_category(iface, category_url, user_agent):
     response = get_category_page_content(iface, category_url, user_agent)
     parsed = parse(response.text)
-    save_category(parsed.raw_json)
+    return parsed
 
 
 def get_category_page_content(iface, category_url, user_agent):
@@ -117,7 +117,6 @@ def get_category_page_content(iface, category_url, user_agent):
                   'application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,'
                   'application/signed-exchange;v=b3;q=0.7'
     }
-
     return get_session(iface['name']).get(category_url, headers=headers, allow_redirects=True, timeout=120)
 
 
@@ -126,12 +125,9 @@ def parse(raw_content):
         soup = BeautifulSoup(raw_content, 'html.parser')
         data = soup.find(id="__NEXT_DATA__").string
         raw_json = json.loads(data)
-
         pagination_v2 = raw_json['props']['pageProps']['initialData']['searchResult']['paginationV2']
-
         max_page = pagination_v2['maxPage']
         current_page = pagination_v2['pageProperties']['page']
-
         return CategoryPageParseResult(
             raw_json=json.dumps(raw_json),
             max_page=max_page,
@@ -147,7 +143,6 @@ def save_category(payload):
         'x-api-key': 'b9e0cfc7-9ba4-43b9-b38f-3191d1f8d686',
         'Content-Type': 'application/json'
     }
-
     try:
         response = requests.post('https://core-data-api.threecolts.com/raw-walmart/categories', headers=headers,
                                  data=payload, timeout=360)
@@ -173,25 +168,19 @@ def restart(interface):
 def process_top_level_categories(categories, user_agents):
     interfaces = get_network_interfaces()
     interfaces_len = len(interfaces)
-
     print('INTERFACES: ' + str(interfaces_len))
     partitioned_categories = split_list(categories, interfaces_len)
-
-    threads = []
+    all_results = []
 
     for i in range(interfaces_len):
         interface = interfaces[i]
         categories_for_interface = partitioned_categories[i]
-        thread = threading.Thread(
-            target=process_categories,
-            args=(interface, categories_for_interface, user_agents),
-            name=f"Thread-{i + 1}"
-        )
-        thread.start()
-        threads.append(thread)
+        results = process_categories(interface, categories_for_interface, user_agents)
+        all_results.extend(results)  # Collect results from each interface
 
-    for thread in threads:
-        thread.join()
+    # Process all results, e.g., save them to API
+    for result in all_results:
+        save_category(result.raw_json)  # Assuming `save_category` can handle the raw_json directly
 
     for iface in interfaces:
         restart(iface['name'])
