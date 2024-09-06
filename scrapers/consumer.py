8ae54f3baefa4fb98d3596f01d710fdc2c599127
ubaklan <ubaklan@threecolts.com>
2024-09-06 22:17:13 +0200

    WIP

--- scrapers/consumer.py
+++ scrapers/consumer.py
@@ -202,5 +202,5 @@ if __name__ == '__main__':
     top_level_all_categories = read_file_to_array('resources/categories.csv')
     top_level_user_agents = read_file_to_array('resources/user_agents.csv')
 
-    for top_level_categories in split_list(top_level_all_categories, 100):
+    for top_level_categories in split_list(top_level_all_categories, 300):
         process_top_level_categories(top_level_categories, top_level_user_agents)
