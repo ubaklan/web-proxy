6a5a6cf4ff3d92a6a69d9e487ae9edaea8dc765b
ubaklan <ubaklan@threecolts.com>
2024-09-11 14:23:48 +0200

    WIP

--- scrapers/slow_consumer.py
+++ scrapers/slow_consumer.py
@@ -117,14 +117,15 @@ if __name__ == '__main__':
     top_level_user_agents = read_file_to_array('resources/user_agents_new.csv')
     top_level_categories = read_file_to_array('resources/categories.csv')
 
-    for category in top_level_categories:
-        try:
-            response = get_category_page_content(
-                get_network_interfaces()[0],
-                category,
-                random.choice(top_level_user_agents)
-            )
-            save_category(response.text)
-        except Exception as e:
-            print('Exception received: ' + str(e))
-            time.sleep(60)
+    while True:
+        for category in top_level_categories:
+            try:
+                response = get_category_page_content(
+                    get_network_interfaces()[0],
+                    category,
+                    random.choice(top_level_user_agents)
+                )
+                save_category(response.text)
+            except Exception as e:
+                print('Exception received: ' + str(e))
+                time.sleep(60)
